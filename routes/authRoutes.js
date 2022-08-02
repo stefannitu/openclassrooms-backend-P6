eeea369b6f395c0507e17546f43b8d81bc960e83
stefan <stefan.nitu.ns@gmail.com>
2022-08-02 16:19:42 +0100

    Project 6 Restart

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// destructuring login from controllers
-const { login, signup } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/signup', signup);
-
-module.exports = router;
